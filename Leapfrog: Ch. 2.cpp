378a0762bb25ae847338f2f011994cb15067ba04
Ahmad-Ali-Sherazi <44944576+Ahmad-Ali-Sherazi@users.noreply.github.com>
2019-06-17 14:41:47 +0500

    Solution of Leapfrog: Ch. 2

---
+++ Leapfrog: Ch. 2.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	ifstream in;
+	ofstream out;
+	int t;
+	int counts, countb;
+	char n;
+	in.open("leapfrog_ch_.txt");
+	out.open("output.txt");
+	in >> t;
+	in >> n;
+	for (int i = 0; i<t ; i++)
+	{
+		counts = 0;
+		countb = 0;
+		if (n == 'A')
+		{
+			in >> n;
+			while (true)
+			{
+				if (n == 'B')
+				{
+					countb++;
+				}
+				else if (n == '.')
+				{
+					counts++;
+				}
+				else if (n == 'A')
+				{
+					break;
+				}
+				in >> n;
+			}
+			//Solution for Leapfrog: Ch. 2
+			if (countb >= 2 && counts >= 1)
+			{
+				cout << "Case #" << i + 1 << ": Y";
+				out << "Case #" << i + 1 << ": Y";
+			}
+			else if (countb >= counts && counts > 0)
+			{
+				cout << "Case #" << i + 1 << ": Y";
+				out << "Case #" << i + 1 << ": Y";
+			}
+			else
+			{
+				cout << "Case #" << i + 1 << ": N";
+				out << "Case #" << i + 1 << ": N";
+			} 
+		}  
+	}
+	in.close();
+	out.close();
+	return 0;
+}
